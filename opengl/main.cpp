45f7fac7322a0e584d7b9f2c99a4245a4062728a
Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
2015-02-17 00:26:24 +0900

    first commit

---
+++ opengl/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  opengl
+//
+//  Created by tsuchida yuuki on 2015/02/16.
+//  Copyright (c) 2015年 tsuchida yuuki. All rights reserved.
+//
+
+#include <iostream>
+#include <functional>
+#include <GLFW/glfw3.h>
+
+void a( int b )
+{
+	
+}
+int main(int argc, const char * argv[]) {
+	
+	auto p = []{};
+	
+    return 0;
+}
